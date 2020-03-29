ad256aebe524d5ce6b729a74e36b2b108bbe5932
Emil Backlund <emil.b@bokio.se>
2020-03-29 23:54:47 +0200

    Adding avalonia app with support for charting through oxyplot

---
+++ src/PortfolioRebalancer/PortfolioRebalancer.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioRebalancer.App.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to PortfolioRebalancer";
+    }
+}
---
+++ src/PortfolioRebalancer/PortfolioRebalancer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PortfolioRebalancer
+{
+    public class Class1
+    {
+    }
+}
