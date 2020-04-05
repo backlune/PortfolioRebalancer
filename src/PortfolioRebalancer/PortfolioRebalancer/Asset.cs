8425954085232e0b040477e307f003cac5e2778a
Emil Backlund <emil.b@bokio.se>
2020-04-05 22:54:36 +0200

    Using fetcher but not correctly parsing yet.

--- src/PortfolioRebalancer/PortfolioRebalancer/Asset.cs
+++ src/PortfolioRebalancer/PortfolioRebalancer/Asset.cs
@@ -12,5 +12,18 @@ namespace PortfolioRebalancer
         public decimal ValueDomesticCurrency { get; set; }
         public string Tag { get; set; }
 
+        public static PortfolioAsset CreateFromRawData(string name, string units, string unitPrice, string valueSEK)
+        {
+            var asset = new PortfolioAsset { Name = name };
+            asset.Identifier = name; // TODO EB (2020-04-05): Maybe change, fetch a different identifier
+
+            // TODO EB (2020-04-05): This will not be enough have seen 500 USD and similar here.
+            asset.Units = Decimal.Parse(units);
+            asset.UnitPrice = Decimal.Parse(unitPrice);
+            asset.ValueDomesticCurrency = Decimal.Parse(valueSEK);
+
+            return asset;
+        }
+
     }
 }
